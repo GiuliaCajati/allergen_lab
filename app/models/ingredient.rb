44b6f99277c779884843739466f224d97945d165
giuliacajati <giuliacajati@gmail.com>
2020-09-20 19:40:50 -0400

    Adjustments to index.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,6 @@
 class Ingredient < ApplicationRecord
-    has_many :recipes, through: :recipeingredients
-    has_many :recipeingredients
+    has_many :recipe_ingredients
+    has_many :recipes, through: :recipe_ingredients
     has_many :allergies
     has_many :users, through: :allergies 
 
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,5 @@
 class Recipe < ApplicationRecord
-    has_many :ingredients, through: :recipeingredients
     has_many :recipeingredients
+    has_many :ingredients, through: :recipeingredients
     belongs_to :user 
 end
